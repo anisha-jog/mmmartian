2130c8d2de86b5ec19476f8ed36191ef4d54f799
D <shaomq21@gmail.com>
2026-02-26 02:19:03 -0500

    ii

--- lab2/ik_ros.py
+++ lab2/ik_ros.py
@@ -94,9 +94,10 @@ def move_to_grasp_goal(target_point, target_orientation):
     q_soln = chain.inverse_kinematics(target_point, target_orientation, orientation_mode='all', initial_position=q_init)
     print('Solution:', q_soln)
     err = np.linalg.norm(chain.forward_kinematics(q_soln)[:3, 3] - np.array(target_point))
-    if not np.isclose(err, 0.0, atol=1e-2):
-        print("IKPy did not find a valid solution")
+    if not np.isclose(err, 0.0, atol=5e-2):
+        print("IKPy did not find a valid solution (position error %.4f m)" % err)
         return None
+    print("执行动作中...")
     move_to_configuration(q=q_soln)
     return q_soln
 
