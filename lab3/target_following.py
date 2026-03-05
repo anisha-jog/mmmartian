ab12c57d46e04a755014f5fb708c4b944034eaba
hea467 <tianhui2@andrew.cmu.edu>
2026-03-05 18:30:48 -0500

    new changes

--- lab3/target_following.py
+++ lab3/target_following.py
@@ -109,7 +109,7 @@ class IKTargetFollowing(HelloNode):
             action = q_soln.copy()
             action[1] = action[1] - ik._base_rotation_accum # scale up arm lift since its a smaller range of motion
             action[2] = action[2] - ik._base_translation_accum
-            ik.update_base_accum(self, q_soln)
+            ik.update_base_accum(q_soln[1], q_soln[2])
             ik.move_to_configuration(self, action)
         
 
