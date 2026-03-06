5f300c9e5f92fa5d35467efb809f145990641a7c
hea467 <tianhui2@andrew.cmu.edu>
2026-03-05 19:46:06 -0500

    new changes

--- lab3/ik_ros_utils.py
+++ lab3/ik_ros_utils.py
@@ -148,7 +148,7 @@ def get_grasp_goal(target_point, target_orientation, q_init):
     # previously the move_to_grasp() function from lab 2
     #   moved to its own function without the final move_to_configuration() call for convenience in this lab
     # print(q_init)
-    q_soln = chain.inverse_kinematics(target_point, target_orientation, orientation_mode='all', initial_position=q_init)
+    q_soln = chain.inverse_kinematics(target_point, target_orientation, initial_position=q_init)
     # print('Solution:', q_soln)
     print("Solution Calculated")
 
