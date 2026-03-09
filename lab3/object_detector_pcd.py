bc8abdc108af3eb5ae101a135b02a98c472c94ca
hea467 <tianhui2@andrew.cmu.edu>
2026-03-09 19:41:51 -0400

    new

--- lab3/object_detector_pcd.py
+++ lab3/object_detector_pcd.py
@@ -90,8 +90,7 @@ class YOLOEObjectDetector(Node):
             self.goal_pose_msg = None
             return None
         xyz[0] += 0.15
-        xyz[1] += 0.1
-        xyz[2] += 0.05
+        xyz[2] += 0.1
         timestamp = self.latest_color_cam_info.header.stamp
         frame_id = self.latest_color_cam_info.header.frame_id
         print("Goal in camera frame:", xyz)
