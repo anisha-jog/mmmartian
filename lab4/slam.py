08d8bea5ccb0b10c08a08e756591a60c9edcf66f
Anisha Jog <ajog2@illinois.edu>
2026-03-11 15:52:36 -0400

    Update slam.py

--- lab4/slam.py
+++ lab4/slam.py
@@ -8,3 +8,7 @@ from hello_helpers.hello_misc import HelloNode
 
 # build a map: offline_mapping.launch.py
 # map names: martian_map
+# using head camera:
+#    ros2 launch stretch_core d435i_low_resolution.launch.py
+# using rviz:
+# ros2 run rviz2 rviz2 -d `ros2 pkg prefix --share stretch_calibration`/rviz/stretch_simple_test.rviz
