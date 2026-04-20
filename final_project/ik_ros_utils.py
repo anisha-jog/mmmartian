b82bef310e4931de976c72e154ec6275b8db6649
Anisha Jog <ajog2@illinois.edu>
2026-04-20 19:29:36 -0400

    test

--- final_project/ik_ros_utils.py
+++ final_project/ik_ros_utils.py
@@ -156,6 +156,8 @@ def get_current_configuration(joint_state):
         # print("THIS IS A CHANGE")
         names = [l.name for l in chain.links]
         print(len(names), names)
+        for link in chain.links:
+            print(link.name, link.bounds)
         # ['Base link', 'joint_base_rotation', 'joint_base_translation', 'joint_mast', 'joint_lift', 'joint_arm_l4', 'joint_arm_l3', 'joint_arm_l2', 'joint_arm_l1', 'joint_arm_l0', 'joint_wrist_yaw', 'joint_wrist_yaw_bottom', 'joint_wrist_pitch', 'joint_wrist_roll', 'joint_gripper_s3_body', 'joint_gripper_finger_right']
         if name not in names:
             return value
@@ -170,8 +172,8 @@ def get_current_configuration(joint_state):
     q_yaw = bound_range('joint_wrist_yaw', joint_state['joint_wrist_yaw'])
     q_pitch = bound_range('joint_wrist_pitch', joint_state['joint_wrist_pitch'])
     q_roll = bound_range('joint_wrist_roll', joint_state['joint_wrist_roll'])
-    q_grip_r = bound_range('joint_gripper_finger_right', joint_state['joint_gripper_finger_right'])
-    q = [0.0, q_base_rotation, q_base, 0.0, q_lift, 0.0, q_arml, q_arml, q_arml, q_arml, q_yaw, 0.0, q_pitch, q_roll, 0.0, q_grip_r] # added 1 entry at the end for gripper
+    # q_grip_r = bound_range('joint_gripper_finger_right', joint_state['joint_gripper_finger_right'])
+    q = [0.0, q_base_rotation, q_base, 0.0, q_lift, 0.0, q_arml, q_arml, q_arml, q_arml, q_yaw, 0.0, q_pitch, q_roll, 0.0, 0.0] # added 1 entry at the end for gripper
 
     # print("current configuration retrieved")
     return q
